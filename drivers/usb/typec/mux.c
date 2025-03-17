cd863d39a837192cad969dc9b4dce7b68bf283df
Agus Suristyono <sandatjepil@proton.me>
2025-03-18 02:52:07 +0700

    Revert "BACKPORT: usb: typec: mux: Fix matching with typec_altmode_desc"
    This reverts commit 83013edcb6fdd617b325d78097bad2e32e738d19.

--- drivers/usb/typec/mux.c
+++ drivers/usb/typec/mux.c
@@ -189,7 +189,6 @@ static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 	bool match;
 	int nval;
 	u16 *val;
-	int ret;
 	int i;
 
 	if (!con->fwnode) {
@@ -224,10 +223,10 @@ static void *typec_mux_match(struct device_connection *con, int ep, void *data)
 	if (!val)
 		return ERR_PTR(-ENOMEM);
 
-	ret = fwnode_property_read_u16_array(con->fwnode, "svid", val, nval);
-	if (ret < 0) {
+	nval = fwnode_property_read_u16_array(con->fwnode, "svid", val, nval);
+	if (nval < 0) {
 		kfree(val);
-		return ERR_PTR(ret);
+		return ERR_PTR(nval);
 	}
 
 	for (i = 0; i < nval; i++) {
