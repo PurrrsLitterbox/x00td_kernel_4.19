bc25b3f5a805d491965ea6684bc86f688e4b3a41
Agus Suristyono <sandatjepil@proton.me>
2025-04-14 00:45:28 +0700

    power: smb-lib: adapt bypass charger from AxionOS
    * Adapt to match our charging hack method
    * Thanks to rve27 for this amazing patch https://github.com/Rve27/patch-bypass-charging/blob/main/kernel-4.9/0001-k4.9-Implement-bypass-charging.patch
    
    Signed-off-by: Agus Suristyono <sandatjepil@proton.me>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -36,6 +36,8 @@
 #define	START_REPORT_BAT_TEMPRATURE	620
 #endif
 
+static unsigned int bypass_charging = 0;
+
 static unsigned int forced_current = 2500;
 module_param(forced_current, uint, S_IWUSR | S_IRUGO);
 
@@ -1942,9 +1944,13 @@ int smblib_vbus_regulator_is_enabled(struct regulator_dev *rdev)
 int smblib_get_prop_input_suspend(struct smb_charger *chg,
 				  union power_supply_propval *val)
 {
-	val->intval
-		= (get_client_vote(chg->usb_icl_votable, USER_VOTER) == 0)
-		 && get_client_vote(chg->dc_suspend_votable, USER_VOTER);
+	if ((get_client_vote(chg->chg_disable_votable, BYPASS_VOTER) == 1)) {
+    	val->intval = 1;
+    } else if (bypass_charging) {
+    	val->intval = 2;
+    } else {
+    	val->intval = 0;
+    }
 	return 0;
 }
 
@@ -2257,11 +2263,15 @@ int smblib_set_prop_input_suspend(struct smb_charger *chg,
 	int rc;
 
 	/* vote 0mA when suspended */
-	rc = vote(chg->usb_icl_votable, USER_VOTER, (bool)val->intval, 0);
-	if (rc < 0) {
-		smblib_err(chg, "Couldn't vote to %s USB rc=%d\n",
-			(bool)val->intval ? "suspend" : "resume", rc);
-		return rc;
+	if (val->intval == 1) {
+		rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 1, 0);
+		bypass_charging = 0;
+	} else if (val->intval == 2) {
+		rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 0, 0);
+		bypass_charging = 1;
+	} else {
+		rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 0, 0);
+		bypass_charging = 0;
 	}
 
 	rc = vote(chg->dc_suspend_votable, USER_VOTER, (bool)val->intval, 0);
@@ -2302,6 +2312,8 @@ int smblib_set_prop_batt_status(struct smb_charger *chg,
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
 {
+	int fake_temp_level;
+
 	if (val->intval < 0)
 		return -EINVAL;
 
@@ -2313,6 +2325,19 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 
 	chg->system_temp_level = val->intval;
 
+	if (get_client_vote(chg->chg_disable_votable, BYPASS_VOTER) == 1) {
+		pr_info("%s bypass charging enabled",__FUNCTION__);
+		return vote(chg->chg_disable_votable, THERMAL_DAEMON_VOTER, true, 0);
+	}
+
+	if (bypass_charging) {
+		fake_temp_level = chg->system_temp_level-2;
+		if (fake_temp_level < 0) fake_temp_level = 0;
+		pr_info("%s limited charging enabled %d",__FUNCTION__, fake_temp_level);
+		return vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
+			chg->thermal_mitigation[fake_temp_level]);
+	}
+
 #ifndef CONFIG_FORCE_FAST_CHARGE
 	if (chg->system_temp_level == chg->thermal_levels)
 		return vote(chg->chg_disable_votable,
@@ -2321,6 +2346,7 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 
 	vote(chg->chg_disable_votable, THERMAL_DAEMON_VOTER, false, 0);
 //	if (chg->system_temp_level == 0)
+		vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, false, 0);
 		return vote(chg->usb_icl_votable, THERMAL_DAEMON_VOTER, false, 0);
 
 	vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
